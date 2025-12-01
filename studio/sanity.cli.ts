c3dea909cf8da8722474a7658dae037d10b6cb2f
Matthijs Wensveen <matthijs.wensveen@soprasteria.com>
2025-12-01 16:24:58 +0100

    Initial commit

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -17,4 +17,7 @@ export default defineCliConfig({
   },
   studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '', // Visit https://www.sanity.io/docs/environment-variables to leanr more about using environment variables for local & production.
   autoUpdates: true,
+  deployment: {
+    appId: 'ep9gdafxof83gliwlcbtrloq',
+  },
 })
